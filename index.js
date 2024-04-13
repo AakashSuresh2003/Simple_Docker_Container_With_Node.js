065a81a5e45a6a4589568b401dc8722945cdb6ca
Aakash S <aakash@Aakashs-MacBook-Air.local>
2024-04-13 22:14:23 +0530

    Created till get and post request

--- index.js
+++ index.js
@@ -6,18 +6,43 @@ const app = express();
 app.use(express.json());
 
 // Routes
-app.get("/",(req,res)=>{
-    res.sendStatus(200);
+app.get("/", async(req,res)=>{
+    try{
+        const data = await pool.query('SELECT * FROM schools');
+
+        res.sendStatus(200).send(data.rows)
+    }
+    catch(err)
+    {
+        console.log(err);
+        res.sendStatus(500);
+    }
 })
 
-app.post("/",(req,res)=>{
+app.post("/", async(req,res)=>{
     const {name,location} = req.body;
-    res.status(200).send({
-        message: `Your keys were ${name}, ${location}`
-    })
+    try{
+        await pool.query('INSERT INTO schools (name,address) VALUES ($1,$2)',[name,location]);
+        res.sendStatus(200).send({message:"Successfully added child"})
+    }
+    catch(err)
+    {
+        console.log(err);
+        res.sendStatus(500);
+    }
 })
 
-
+app.get("/setup", async (req,res)=>{
+    try{
+        await pool.query('CREATE TABLE schools( id SERIAL PRIMARY KEY, name VARCHAR(100),address VARCHAR(100))');
+        res.sendStatus(200).send({message:"Successfully created table"});
+    }
+    catch(err)
+    {
+        console.log(err);
+        res.sendStatus(500);
+    }
+})
 
 app.listen(PORT,()=>{
     console.log(`Server listening on the PORT ${PORT}`)
