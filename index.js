c521c96ec2e70f29ae6de70f4607b16aa13cb1e0
Aakash S <aakash@Aakashs-MacBook-Air.local>
2024-04-13 21:54:20 +0530

    Created the demo get and post request

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const pool = require("./Database/db")
 const PORT = 3000
 
 const app = express();
+app.use(express.json());
+
+// Routes
+app.get("/",(req,res)=>{
+    res.sendStatus(200);
+})
+
+app.post("/",(req,res)=>{
+    const {name,location} = req.body;
+    res.status(200).send({
+        message: `Your keys were ${name}, ${location}`
+    })
+})
 
 
 
